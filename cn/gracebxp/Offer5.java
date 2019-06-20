c656a571531fe9b41beee9bbd84b37ee2e6cada4
gracebxp <gracebxp@gmail.com>
2019-06-20 16:22:34 +0800

    提交第五道和第六道

---
+++ cn/gracebxp/Offer5.java
@@ -0,0 +1,31 @@
+package cn.gracebxp;
+
+import java.util.Stack;
+
+/**
+ * 用两个栈实现队列
+ * 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+ */
+public class Offer5 {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() throws Exception {
+        if (stack2.isEmpty()){
+            while (!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+
+        if (stack2.isEmpty()){
+            throw new Exception("queur is empty");
+        }
+
+        return stack2.pop();
+    }
+
+}
